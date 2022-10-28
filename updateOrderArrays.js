bbad873ba7adaaa1a9fb429f975395e68c02fdb1
Ricardo Otálora Luna <ruperto1@protonmail.com>
2022-10-28 10:11:40 -0500

    update & order

---
+++ updateOrderArrays.js
@@ -0,0 +1,51 @@
+const data = [
+    {
+        id: "1",
+        name: "Life"
+    },
+    {
+        id: "2",
+        name: "Nature"
+    },
+    {
+        id: "ert",
+        name: "Science"
+    },
+    {
+        id: "hola",
+        name: "Life"
+    },
+    {
+        id: "alegre",
+        name: "Life"
+    },
+    {
+        name: "Science",
+        id: "5luZZfK"
+    },
+    {
+        name: "Life",
+        id: "_ckGaE7"
+    }
+]
+
+let count = {};
+data.forEach((item) => {
+    count[item.name] = (count[item.name] || 0) + 1;
+});
+console.log("Count: ", count)
+//from this -> {Life: 4, Nature: 1, Science: 2}
+/*  to this: 
+   [
+    {name: "Life", count: 4, textSize: count + 1}, 
+    {name: "Nature", count: 1, textSize: count + 1}, 
+    {name: "Science", count: 2, textSize: count + 1}
+   ]
+*/
+let newArray = [];
+// {Life: 4, Nature: 1, Science: 2}
+for (let name in count) {
+    newArray.push({ name: name, count: count[name], textSize: count[name] + 5 })
+}
+console.log("New Array: ", newArray)
+
