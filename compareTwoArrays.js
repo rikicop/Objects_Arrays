a776699a471c3722241bac2fb383c5b4ec86e260
Ricardo Otálora Luna <ruperto1@protonmail.com>
2022-12-01 16:43:41 -0500

    compare two arrays and substr

--- compareTwoArrays.js
+++ compareTwoArrays.js
@@ -1,13 +1,13 @@
 let cart = [
-    { "id": 5, "color": "white", "type": "sport", "amount": 7 },
-    { "id": 3, "color": "yellow", "type": "pickup", "amount": 8 },
+    { "id": "a5", "color": "white", "type": "sport", "amount": 7 },
+    { "id": "b3", "color": "yellow", "type": "pickup", "amount": 8 },
 ]
 
 let autos = [
-    { "id": 1, "color": "purple", "type": "minivan", "stock": 7 }, { "id": 2, "color": "red", "type": "station_wagon", "stock": 5 },
-    { "id": 3, "color": "yellow", "type": "pickup", "stock": 66 }, { "id": 4, "color": "black", "type": "sedan", "stock": 3 },
-    { "id": 5, "color": "white", "type": "sport", "stock": 55 }, { "id": 6, "color": "blue", "type": "race", "stock": 27 },
-    { "id": 7, "color": "aqua", "type": "taxi", "stock": 9 }
+    { "id": "1c", "color": "purple", "type": "minivan", "stock": 7 }, { "id": "2d", "color": "red", "type": "station_wagon", "stock": 5 },
+    { "id": "b3", "color": "yellow", "type": "pickup", "stock": 66 }, { "id": "4f", "color": "black", "type": "sedan", "stock": 3 },
+    { "id": "a5", "color": "white", "type": "sport", "stock": 55 }, { "id": "6g", "color": "blue", "type": "race", "stock": 27 },
+    { "id": "7h", "color": "aqua", "type": "taxi", "stock": 9 }
 ]
 
 //STOCKS DE CARTS
@@ -19,16 +19,17 @@ autos.forEach(ele => {
     }
 });
 
+let result = []
 //STOCKS DE AUTOS
 console.log("STOCKS DE AUTOS")
 cart.forEach(ele => {
     let value = autos.find(val => val.id === ele.id);
     if (value) {
-        console.log(value, value.stock)
+        result.push({ "id": value.id, "total": value.stock - ele.amount })
     }
 });
 
-
+console.log("result: ", result)
 /* autos.forEach((item, index) => {
     if (cart[index] === undefined) {
         cart[index] = {
